f76086571a31e6b82348fa5cb08c03dcf9a1426c
chaninkou <michaelkoulove@yahoo.com>
2020-01-28 22:44:48 -0800

    Great question on using Integer.MAX_VALUE to find minimum.

---
+++ src/increasingThree/CheckIncreasingThreeSubsequenceFunction.java
@@ -0,0 +1,24 @@
+package increasingThree;
+
+public class CheckIncreasingThreeSubsequenceFunction {
+	public boolean increasingTriplet(int[] nums) {
+		// This will store the min, second min number
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        
+        for(int num : nums){
+        	// Should get the min first or else this is not going to work
+            if(num <= min){
+                min = num;
+            } else if (num <= secondMin){ // If min is already found, set second min now
+                secondMin = num;
+            } else if (num > secondMin) { // Could just put "else" but this is more clear
+            	// That means the last number is bigger than min and second min
+                return true;
+            }
+        }
+        
+        // If its here, that means no increasing was found
+        return false;
+    }
+}
---
+++ src/increasingThree/Main.java
@@ -0,0 +1,5 @@
+package increasingThree;
+
+public class Main {
+
+}
